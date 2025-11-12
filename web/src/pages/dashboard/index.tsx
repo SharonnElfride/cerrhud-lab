3e5bfd825e7ea7cd585c059bf4ee0c4f12c282da
Sharonn <sharonn@Mac.lan>
2025-11-12 23:31:14 +0100

    Dashboard molding.

--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -1,8 +1,25 @@
+import EntityCard from "@/components/dashboard/EntityCard";
 import ListTitle from "@/components/shared/ListTitle";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item";
+import { Spinner } from "@/components/ui/spinner";
 import { useAuth } from "@/context/AuthContext";
+import { ProfileRoute, UsersRoute } from "@/navigation/app_routes";
+import { canAccessRoute } from "@/navigation/guards";
+import { UserCogIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = ({}) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+  const navigate = useNavigate();
 
   return (
     <div className="p-5 space-y-5">
@@ -10,12 +27,47 @@ const Dashboard = ({}) => {
         title="Tableau de bord"
         description="Vue d'ensemble des statistiques et activités récentes du système."
       />
-      <h1 className="text-2xl font-semibold mb-4">
-        Welcome, {user?.first_name + " - " + user?.email}
-      </h1>
-      <p className="text-muted-foreground mb-6">
-        ID: {user?.id} | Role: {user?.role}
-      </p>
+
+      <Item variant="outline">
+        <ItemMedia>
+          <Avatar className="size-10">
+            <AvatarImage src={user?.avatar ?? undefined} alt="avatar" />
+            <AvatarFallback
+              style={{
+                backgroundColor: user?.profile_color ?? "var(--color-primary)",
+                color: "white",
+                fontWeight: 500,
+              }}
+            >
+              {loading ? <Spinner /> : user?.first_name?.charAt(0) ?? "X"}
+            </AvatarFallback>
+          </Avatar>
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle>
+            {user?.first_name} {user?.surname}
+          </ItemTitle>
+          {/* Date / Time lapse */}
+          <ItemDescription>Dernière connexion : XXXX</ItemDescription>
+        </ItemContent>
+        <ItemActions>
+          <Button
+            size="icon-sm"
+            variant="outline"
+            className="rounded-full"
+            onClick={() => {
+              navigate(ProfileRoute.path);
+            }}
+          >
+            <UserCogIcon />
+          </Button>
+        </ItemActions>
+      </Item>
+
+      <div className="flex w-full gap-2">
+        <EntityCard entityType="medical_tests" />
+        {canAccessRoute(UsersRoute, user) && <EntityCard entityType="users" />}
+      </div>
     </div>
   );
 };
